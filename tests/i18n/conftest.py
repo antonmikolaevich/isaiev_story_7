3d209ff7c8ff273289776ddff5c6eb1f6a955758
antonmikolaevich <57948151+antonmikolaevich@users.noreply.github.com>
2026-03-11 23:47:24 +0100

    Delete tests/i18n/conftest.py

--- tests/i18n/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest fixtures and helpers for i18n tests.
-
-This file is created incrementally. It contains basic imports and docstring.
-"""
-
-import os
-
-# Additional fixtures and helpers will be added in an edit step.
