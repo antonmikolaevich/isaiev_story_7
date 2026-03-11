f5b69a99c770e1689e5a35f82b7a51f75e816098
antonmikolaevich <57948151+antonmikolaevich@users.noreply.github.com>
2026-03-11 23:46:46 +0100

    Create tests/i18n/conftest.py

---
+++ tests/i18n/conftest.py
@@ -0,0 +1,8 @@
+"""Pytest fixtures and helpers for i18n tests.
+
+This file is created incrementally. It contains basic imports and docstring.
+"""
+
+import os
+
+# Additional fixtures and helpers will be added in an edit step.
